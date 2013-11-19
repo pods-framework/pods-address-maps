63e3e9e1cf29ad16d977794ce3a9c7e9c48ed7fa
Dan Stefan Oprean <dan@designxenon.com>
2013-11-19 06:48:28 +0200

    Add fields / labls in map.php field, fix javascript so it works.

--- ui/fields/map.php
+++ ui/fields/map.php
@@ -1,23 +1,27 @@
 <?php
-	wp_register_script( 'googlemaps', 'http://maps.googleapis.com/maps/api/js?sensor=false', false, '3' );
-	wp_enqueue_script( 'googlemaps' );
-?>
-<div id="map_canvas" style="width: 95%; height: 300px;"></div>
-<label for="">Address:</label>
-<input name="pods_field_map_address" data-name-clean="pods-field-map-address" id="pods-form-ui-pods-field-map-address" class="pods-form-ui-field-type-map pods-form-ui-field-name-pods-field-map-test" type="text" tabindex="2">
-<input type="button" name="pods_field_map_geocode" value="Lookup Location from Address" />
-<label for="">Latitude:</label>
-<input type="text" name="pods_field_map_lat" disabled="disabled" />
-<label for="">Longitude:</label>
-<input type="text" name="pods_field_map_lng" disabled="disabled" />
+wp_enqueue_script( 'googlemaps' );
+wp_enqueue_style( 'pods-component-address-maps' );
 
+$attributes = array();
+$attributes = PodsForm::merge_attributes( $attributes, $name, $form_field_type, $options );
+?>
+<div id="<?php echo $attributes[ 'id' ] . '-map-canvas' ?>" class="pods-address-maps-map-canvas"></div>
+
+<?php echo PodsForm::label( $name . '-map-lookup-address', 'Address to lookup:' ) ?>
+<?php echo PodsForm::field( $name . '-map-lookup-address', '', 'text' ) ?>
+<input type="button" name="<?php echo $attributes[ 'id' ] . '-map-lookup-button' ?>" id="<?php echo $attributes[ 'id' ] . '-map-lookup-button' ?>" value="Lookup Location from Address" />
+<?php echo PodsForm::label( $name . '-map-lookup-lat', 'Latitude' ) ?>
+<?php echo PodsForm::field( $name . '-map-lookup-lat', '', 'text', array( 'read_only' => 1 ) ) ?>
+
+<?php echo PodsForm::label( $name . '-map-lookup-lon', 'Longitude' ) ?>
+<?php echo PodsForm::field( $name . '-map-lookup-lon', '', 'text', array( 'read_only' => 1 ) ) ?>
 <script type="text/javascript">
-	jQuery(document).ready(function($) {
-		var mapDiv = document.getElementById('map_canvas');
-		var geocodeButton = $('input[name=pods_field_map_geocode]');
-		var addressField = $('input[name=pods_field_map_address]');
-		var latField = $('input[name=pods_field_map_lat]');
-		var lngField = $('input[name=pods_field_map_lng]');
+	jQuery( document ).ready( function ( $ ) {
+		var mapDiv = document.getElementById( '<?php echo $attributes['id']  . '-map-canvas'?>' );
+		var geocodeButton = $( '#<?php echo $attributes['id'] . '-map-lookup-button'  ?>' );
+		var addressField = $( '#<?php echo $attributes['id']  . '-map-lookup-address'  ?>' );
+		var latField = $( '#<?php echo $attributes['id']  . '-map-lookup-lat'  ?>' );
+		var lngField = $( '#<?php echo $attributes['id']  . '-map-lookup-lon'  ?>' );
 
 		var map = null;
 		var geocoder = null;
@@ -26,15 +30,15 @@
 		//------------------------------------------------------------------------
 		// Initialze the map
 		//
-		(function() {
+		(function () {
 			var zoom = 10;
-			var center = new google.maps.LatLng(40.026, -82.936);
+			var center = new google.maps.LatLng( 40.026, -82.936 );
 
-			map = new google.maps.Map(mapDiv, {
-				center: center,
-				zoom: zoom,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			});
+			map = new google.maps.Map( mapDiv, {
+				center : center,
+				zoom : zoom,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			} );
 
 			geocoder = new google.maps.Geocoder();
 		})();
@@ -42,45 +46,46 @@
 		//------------------------------------------------------------------------
 		// Geolocate from the address
 		//
-		geocodeButton.click(function() {
+		geocodeButton.on( 'click', function ( event ) {
+			event.preventDefault();
 			var address = addressField.val();
 
-			geocoder.geocode( { 'address': address}, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					location = results[0].geometry.location;
+			geocoder.geocode( { 'address' : address}, function ( results, status ) {
+				if ( status == google.maps.GeocoderStatus.OK ) {
+					var location = results[0].geometry.location;
 
 					// Center the map and set the lat/lng values
-					map.setCenter(location);
-					latField.val(location.lat());
-					lngField.val(location.lng());
+					map.setCenter( location );
+					latField.val( location.lat() );
+					lngField.val( location.lng() );
 
 					// Set the marker options
 					var markerOptions = {
-						map: map,
-						position: location,
-						draggable: true
+						map : map,
+						position : location,
+						draggable : true
 					};
 
 					// Create a new marker, if needed, and set the event listeners
-					if (!marker) {
-						marker = new google.maps.Marker(markerOptions);
-						google.maps.event.addListener(marker, 'drag', function() {
-							latField.val(marker.getPosition().lat());
-							lngField.val(marker.getPosition().lng());
-						});
+					if ( !marker ) {
+						marker = new google.maps.Marker( markerOptions );
+						google.maps.event.addListener( marker, 'drag', function () {
+							latField.val( marker.getPosition().lat() );
+							lngField.val( marker.getPosition().lng() );
+						} );
 					}
 					// Marker is already set, just update its options
 					else {
-						marker.setOptions(markerOptions);
+						marker.setOptions( markerOptions );
 					}
 				}
 				// Geocode failure
 				else {
-					alert("Geocode was not successful for the following reason: " + status);
+					alert( "Geocode was not successful for the following reason: " + status );
 				}
-			}); // end geocode
-		}); // end button click event
+			} ); // end geocode
+		} ); // end button click event
 
-	}); // end document ready
+	} ); // end document ready
 
 </script>
