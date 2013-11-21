c120d5485afebbbc7a0303298284d1d580d48f5e
Dan Stefan Oprean <dan@designxenon.com>
2013-11-21 04:11:37 +0200

    Whoops, forgot longitude.

--- ui/fields/lat-long.php
+++ ui/fields/lat-long.php
@@ -8,7 +8,7 @@ $attributes = PodsForm::merge_attributes( $attributes, $name, PodsForm::$field_t
 <?php echo PodsForm::label( $name . '_lat', 'Latitude' ); ?>
 <?php echo PodsForm::field( $name . '_lat', $value, 'text', $options ) ?>
 
-<?php echo PodsForm::label( $name . '_lat', 'Latitude' ); ?>
-<?php echo PodsForm::field( $name . '_lat', $value, 'text', $options ) ?>
+<?php echo PodsForm::label( $name . '_lon', 'Longitude' ); ?>
+<?php echo PodsForm::field( $name . '_lon', $value, 'text', $options ) ?>
 <?php
 PodsForm::regex( PodsForm::$field_type, $options );
