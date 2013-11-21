9216732867c29e51cb68bcd88894beecacf44d1e
Dan Stefan Oprean <dan@designxenon.com>
2013-11-21 02:25:34 +0200

    Latitude / longitude input.

--- ui/fields/lat-long.php
+++ ui/fields/lat-long.php
@@ -5,6 +5,10 @@ $attributes[ 'value' ] = $value;
 $attributes[ 'tabindex' ] = 2;
 $attributes = PodsForm::merge_attributes( $attributes, $name, PodsForm::$field_type, $options );
 ?>
-<input<?php PodsForm::attributes( $attributes, $name, PodsForm::$field_type, $options ); ?> />
+<?php echo PodsForm::label( $name . '_lat', 'Latitude' ); ?>
+<?php echo PodsForm::field( $name . '_lat', $value, 'text', $options ) ?>
+
+<?php echo PodsForm::label( $name . '_lat', 'Latitude' ); ?>
+<?php echo PodsForm::field( $name . '_lat', $value, 'text', $options ) ?>
 <?php
 PodsForm::regex( PodsForm::$field_type, $options );
